7dde38e17dd96cc292bd6c232a6517937821aabf
Paolo Phan <phudev95@gmail.com>
2025-12-15 00:19:01 +0700

    update source

--- src/searching/LinearSearch/LinearSearch.test.ts
+++ src/searching/LinearSearch/LinearSearch.test.ts
@@ -1,13 +1,15 @@
 import { solution } from "./LinearSearch";
 
 describe("Linear Search", () => {
-  it.each`
-    A                     | F      | expectedResult
-    ${[2, 6, 7, 90, 103]} | ${90}  | ${3}
-    ${[2, 6, 7, 90, 103]} | ${103} | ${4}
-    ${[2, 6, 7, 90, 103]} | ${8}   | ${-1}
-    ${[]}                 | ${8}   | ${-1}
-  `("should return correct value", ({ A, F, expectedResult }) => {
-    expect(solution(A, F)).toStrictEqual(expectedResult);
-  });
+  it.each([
+    { A: [2, 6, 7, 90, 103], F: 90, expectedResult: 3 },
+    { A: [2, 6, 7, 90, 103], F: 103, expectedResult: 4 },
+    { A: [2, 6, 7, 90, 103], F: 8, expectedResult: -1 },
+    { A: [], F: 8, expectedResult: -1 },
+  ])(
+    "should return correct value for A=$A, F=$F",
+    ({ A, F, expectedResult }) => {
+      expect(solution(A, F)).toStrictEqual(expectedResult);
+    }
+  );
 });
